7f2a325a82b28bbdc5ba2428f25ba5047fd69a1d
grigor-aramyan <grigori.aramyan@gmail.com>
2017-12-13 20:38:32 +0400

    switch to noredink pipeline json decode lib

--- web/elm/Decoders.elm
+++ web/elm/Decoders.elm
@@ -2,28 +2,29 @@ module Decoders exposing (..)
 
 import Json.Decode as JD exposing (..)
 import Json.Decode.Extra exposing ((|:))
+import Json.Decode.Pipeline exposing (decode, required, optional)
 import Model exposing (..)
 
 contactListDecoder : JD.Decoder ContactList
 contactListDecoder =
-    succeed
+    decode
         ContactList
-                |: (field "entries" (list contactDecoder))
-                |: (field "page_number" int)
-                |: (field "total_entries" int)
-                |: (field "total_pages" int)
+            |> required "entries" (list contactDecoder)
+            |> required "page_number" int
+            |> required "total_entries" int
+            |> required "total_pages" int
 
 contactDecoder : JD.Decoder Contact
 contactDecoder =
-    succeed
+    decode
         Contact
-        |: (field "id" int)
-        |: (field "first_name" string)
-        |: (field "last_name" string)
-        |: (field "gender" int)
-        |: (field "birth_date" string)
-        |: (field "location" string)
-        |: (field "phone_number" string)
-        |: (field "email" string)
-        |: (field "headline" string)
-        |: (field "picture" string)
+            |> required "id" int
+            |> required "first_name" string
+            |> required "last_name" string
+            |> required "gender" int
+            |> required "birth_date" string
+            |> required "location" string
+            |> optional "phone_number" string "748397483"
+            |> required "email" string
+            |> optional "headline" string "some headline"
+            |> optional "picture" string "https://randomuser.me/api/portraits/women/33.jpg"
